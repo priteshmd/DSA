548d7fe0e68ef529ad6ec813bd6d66c364aecc5f
Pritesh Dhakate <priteshmd@gmail.com>
2025-11-20 22:53:23 +0530

    Time: 4 ms (100.00%) | Memory: 45.9 MB (17.96%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,37 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int expectednum = x;
-        int reversenum = 0;
-        int[] nums = new int[5000];
-        if(x < 0){
+       if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        for(int i = 0; x != 0; i++)
-        {
-            nums[i] = (x % 10);
-             System.out.println(nums[i]);
-             reversenum = reversenum * 10 + nums[i];
-            x = x/10;
+
+        int revertedNumber = 0;
+
+        while (x > revertedNumber) {
+            // Get the last digit of x and append it to revertedNumber.
+            revertedNumber = revertedNumber * 10 + (x % 10);
+            
+            // Remove the last digit from x.
+            x /= 10;
         }
-       
-            if(reversenum != expectednum ){
-                return false;
-            }
-         
-        System.out.println(reversenum + " "   + expectednum);
-return true;
+
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
 
-        
-
-    //     int n = nums.length;
-    //     for(int i=0; i<=n/2 ; i++){
-    //         if(nums[i] != nums[n-1-i]){
-    //             return false;
-    //         }
-            
-    //     }
-    //     return true;
-    // }
-    
